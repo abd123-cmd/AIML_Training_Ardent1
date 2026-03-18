fd47b5f0e420382aa3855edff34e7176d6e9f2fc
abd123-cmd <souhardyabose3619@gmail.com>
2026-03-18 22:17:22 +0530

    Add files via upload

---
+++ Student_Intro_Generator.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+course = input("Enter your course: ")
+college = input("Enter your college: ")
+hobby = input("Enter your hobby: ")
+
+intro = f"""
+Hello! My name is {name}.
+I am {age} years old.
+I am currently studying {course} at {college}.
+In my free time, I enjoy {hobby}.
+Nice to meet you!
+"""
+
+print(intro)
